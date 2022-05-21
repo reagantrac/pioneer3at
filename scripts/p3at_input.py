bd095e3be2b3c4dad5af7acd3a5dcf2fafc06c5a
reagantrac <reagantrac@gmail.com>
2022-05-21 14:44:02 +0800

    Controller input

--- scripts/p3at_input.py
+++ scripts/p3at_input.py
@@ -6,52 +6,86 @@ from geometry_msgs.msg import Vector3
 from evdev import list_devices, InputDevice, categorize, ecodes
 
 stick_min = 10000
-stick_max = 53000
+stick_max = 52000
+
+drive_vec = Vector3()
+key_pressed = ""
+alive = False
 
 def read_controller(dev):
-	event = read_one()
-	if event == None: return
-	# button
-	if event.type == ecodes.EV_KEY:
-		print("button", event.code, event.value)
+	global key_pressed, drive_vec
 
-	#read stick axis movement
-	elif event.type == ecodes.EV_ABS:
-		
-		print("stick", event.code, event.value)
+	
+	while dev.read_one() != None: 
+		event = dev.read_one()
+		if event == None:
+			return
+		# button
+		if event.type == ecodes.EV_KEY:
+			
+			print("button", event.code, event.value)
+
+		#read stick axis movement
+		elif event.type == ecodes.EV_ABS:
+			if event.code == 0:
+				y = event.value - (stick_max)/2 - stick_min
+				y /= stick_max
+				drive_vec.y = round(-y*4)/4
+				key_pressed = "k"
+			if event.code == 1:
+				x = event.value - (stick_max)/2 - stick_min
+				x /= stick_max
+				drive_vec.x = round(-x*4)/4
+		key_pressed = "k"
 
 def keypress(data):
-	p = rospy.Publisher("/p3at/keep_alive", Bool, queue_size=1)
-	p.publish(True)
+	global key_pressed, drive_vec
 	
-	v = Vector3()
-	if data.data == "w": v.x = 1
-	elif data.data == "s": v.x = -1
+	if data.data == "w": drive_vec.x = 1
+	elif data.data == "s": drive_vec.x = -1
+	else: drive_vec.x = 0
 	
-	if data.data == "a": v.y = 1
-	elif data.data == "d": v.y = -1
+	if data.data == "a": drive_vec.y = 1
+	elif data.data == "d": drive_vec.y = -1
+	else: drive_vec.y = 0
 	
-	m = rospy.Publisher("/p3at/manual_cmd", Vector3, queue_size=1)
-	m.publish(v)
-	
-	if data.data == " ":
-		s = rospy.Publisher("/p3at/switch_mode", Bool, queue_size=1)
-		s.publish(True)
-	
-	s = rospy.Publisher("/p3at/ui_cmd", String, queue_size=1)
-	s.publish(data.data)
+	key_pressed = data.data
 
 def read_input(dev):
-	rospy.Subscriber('/p3at/key_input', String, keypress)
+	global key_pressed, drive_vec
+
+	ui = rospy.Publisher("/p3at/ui_cmd", String, queue_size=1)
+	cmd = rospy.Publisher("/p3at/manual_cmd", Vector3, queue_size=1)
+	mode = rospy.Publisher("/p3at/switch_mode", Bool, queue_size=1)
+	alive = rospy.Publisher("/p3at/keep_alive", Bool, queue_size=1)
+
+	#rospy.Subscriber('/p3at/key_input', String, keypress)
 	rospy.init_node("p3at_input")
 	
 	
-	rospy.spin()
+	
+	rate = rospy.Rate(10)
+	
+	while not rospy.is_shutdown():
+		read_controller(dev)
+		if key_pressed == "": 
+			cmd.publish(drive_vec)
+			continue
+			
+		
+		alive.publish(True)
+		ui.publish(key_pressed)
+		cmd.publish(drive_vec)
+		if key_pressed == " ":
+			mode.publish(True)
+			
+		key_pressed = ""
+		rate.sleep()
 
 
 if __name__ == "__main__":
-	dev = InputDevice( list_devices()[0] )
 	try:
+		dev = InputDevice( list_devices()[0] )
 		read_input(dev)
 	except rospy.ROSInterruptExeception:
 		pass
